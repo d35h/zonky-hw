4ffd9bde4260ef61ae18ed4b6cc0d3e6fc8db7a8
d35h <danielzaru@gmail.com>
2019-03-15 01:20:36 +0100

    Polish up code

--- src/main/java/com/example/zonkyhw/domain/Loan.java
+++ src/main/java/com/example/zonkyhw/domain/Loan.java
@@ -3,14 +3,11 @@ package com.example.zonkyhw.domain;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
-@Builder
 public class Loan {
 
-    @NotNull
     private long id;
     private String name;
     private String nickName;
